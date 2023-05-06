4cae4179c9c10bb5fd061932bb15550fffea2a1f
bodova <91661224+bodova@users.noreply.github.com>
2023-05-06 18:33:36 +0200

    Delete ModelInference_Simulation/code directory

--- ModelInference_Simulation/code/TrainTestSplits.m
+++
@@ -1,9 +0,0 @@
-function[Split,idtemp] = Inference_TrainTestSplits(Nsplits,Ptrain);
-
-Split = [];                                         % cell objects
-
-idx = 1;
-idtemp = 1;
-[repTrain,repTest] = getrep_traintest(Ptrain);
-Split{1,idx} = repTrain; 
-Split{2,idx} = repTest;
